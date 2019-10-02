9f1df695e5692161996ac67db1cdfeda5ca7f878
Yifan Wang <yifanwang07@163.com>
2019-10-01 20:01:29 -0400

    Barchart available

--- js/barchart.js
+++ js/barchart.js
@@ -43,11 +43,12 @@ function histogram(key=null) {
     console.log("Selected: " + key);
   }
   var catagorical = ['MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition'];
+  var showHistogram = document.getElementById('showHistogram').checked;//showHistogram
   if (catagorical.includes(key)) {
-    histogramCatogorical(key);
+    histogramCatogorical(key, showHistogram);
     document.getElementById('barchart-bin-info').style.display = 'none'
   } else {
-    histogramLinear(key);
+    histogramLinear(key, showHistogram);
     document.getElementById('barchart-bin-info').style.display = ''
   }
 
@@ -55,7 +56,7 @@ function histogram(key=null) {
   e.innerHTML = key;
 }
 
-function histogramLinear(key = "Id") {
+function histogramLinear(key = "Id", hist=false) {
   // set the dimensions and margins of the graph
   var w = document.documentElement.clientWidth;
   console.log("document.documentElement.clientWidth: " + w);
@@ -89,7 +90,7 @@ function histogramLinear(key = "Id") {
         .call(d3.axisBottom(x));
 
     // Y axis: initialization
-    var y = d3.scaleLinear().range([height, 0]);
+    var y = d3.scaleLinear().range([height, 0]).nice();
     var yAxis = svg.append("g")
 
     // A function that builds the graph for a specific value of bin
@@ -104,19 +105,6 @@ function histogramLinear(key = "Id") {
         i += step;
       }
       actualBins.push(domainMax + 1);
-      // i = 0;
-      // var binnedData = [];
-      // while (i < nBin) {
-      //   binnedData.push([]);
-      //   i += 1;
-      // }
-      // // Bins
-      // for (var j = 0; j < data.length; j++) {
-      //   var index = Math.floor(data[j][key] / step) - 1;
-      //   if (index >= nBin) { index = nBin - 1};
-      //   if (index < 0) { index = 0};
-      //   binnedData[index].push(data[j]);
-      // }
 
       // set the parameters for the histogram
       var histogram = d3.histogram()
@@ -129,7 +117,11 @@ function histogramLinear(key = "Id") {
       console.log(bins);
 
       // Y axis: update now that we know the domain
-      y.domain([0, d3.max(bins, function(d) { return +d.length; })]);
+      if (hist) {
+        y.domain([0, d3.max(bins, function(d) { return +d.length; })]);
+      } else {
+        y.domain([0, 800000]);
+      }
       yAxis.transition().duration(1000).call(d3.axisLeft(y));
 
       // Join the rect with the bins data
@@ -181,8 +173,22 @@ function histogramLinear(key = "Id") {
             var val = x(d.x1) - x(d.x0) - 1;
             if (val < 0) { return 0; }
             return x(d.x1) - x(d.x0) - 1 ; })
-          .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-          .attr("height", function(d) { return height - y(d.length); })
+          .attr("transform", function(d) {
+            if (hist) {
+              return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+            }
+            return "translate(" + x(d.x0) + "," + y(d3.max(d, function(d) { return +d.SalePrice; })) + ")"; })
+          .attr("height", function(d) {
+            if (hist) {
+              return height - y(d.length);;
+            }
+            var yyy = d3.max(d, function(d) {
+              return +d.SalePrice; })
+            if (yyy) {
+              return height - y(yyy);
+            }
+            return 0;
+          })
           .style("fill", "#69b3a2")
       // If less bar in the new histogram, I delete the ones not in use anymore
       u.exit().remove()
@@ -200,7 +206,7 @@ function histogramLinear(key = "Id") {
 
 }
 
-function histogramCatogorical(key = "MSZoning") {
+function histogramCatogorical(key = "MSZoning", hist=false) {
   // set the dimensions and margins of the graph
   var w = document.documentElement.clientWidth;
   console.log("document.documentElement.clientWidth: " + w);
@@ -221,73 +227,145 @@ function histogramCatogorical(key = "MSZoning") {
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   // get the data
-  var filename = 'data/Frequency/' + key + ".csv"
-  d3.csv(filename, function(data) {
-    // X axis: scale and draw:
-    var x = d3.scaleBand()
-        .domain(data.map(function(d) {
-          return d.key;
-        }))
-        .range([1, width]);
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+  if (hist) {
+    var filename = 'data/Frequency/' + key + ".csv"
+    d3.csv(filename, function(data) {
+      // X axis: scale and draw:
+      var x = d3.scaleBand()
+          .domain(data.map(function(d) {
+            return d.key;
+          }))
+          .range([1, width]);
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
 
-    // Y axis: initialization
-    var y = d3.scaleLinear()
+      // Y axis: initialization
+      var y = d3.scaleLinear()
+          .domain([0, d3.max(data, function(d) {
+            return +d.frequency;
+          })])
+          .range([height, 0]).nice();
+      var yAxis = svg.append("g");
+
+      // A function that builds the graph for a specific value of bin
+      function update() {
+
+        // set the parameters for the histogram
+        var histogram = d3.histogram()
+            .value(function(d) { return d.key; })   // I need to give the vector of value
+            .domain(x.domain())  // then the domain of the graphic
+            ; // then the numbers of bins
+
+        // And apply this function to data to get the bins
+
+        // Y axis: update now that we know the domain
+        yAxis.transition().duration(1000).call(d3.axisLeft(y));
+
+        // Join the rect with the bins data
+        var u = svg.selectAll(null)
+                    .data(data)
+                    .attr("class", "barchart-bar");
+        // Manage the existing bars and eventually the new ones:
+        u.enter()
+          .append("rect") // Add a new rect for each new elements
+          .on('mouseover', function(d) {
+                d3.select(this).style("fill", HOVERED_BAR_COLOR);
+            })
+          .on('mouseout', function(d) {
+              d3.select(this).style("fill", DEFAULT_BAR_COLOR);
+          })
+          .merge(u) // get the already existing elements as well
+          .transition() // and apply changes to all of them
+          .duration(1000)
+            .attr("x", function(d) { return x(d.key); })
+            .attr("width", function(d) {
+              var w = x.bandwidth() - 1;
+              if (w < 0) { return 0;}
+              return w;
+            })
+            .attr("y", function(d) { return y(d.frequency);})
+            .attr("height", function(d) { return height - y(d.frequency);})
+            .call(d3.axisBottom(x))
+            .style("fill", "#69b3a2");
+        // If less bar in the new histogram, I delete the ones not in use anymore
+        u.exit().remove()
+      }
+
+      // Initialize with 20 bins
+      update()
+
+    });
+  } else {
+    var filename = 'data/Frequency/' + key + ".csv"
+    d3.csv(filename, function(data) {
+      // X axis: scale and draw:
+      var x = d3.scaleBand()
+          .domain(data.map(function(d) {
+            return d.key;
+          }))
+          .range([1, width]);
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+      // Y axis: initialization
+      // var y = d3.scaleLinear().range([height, 0]).domain([0, 800000]).nice();
+      var y = d3.scaleLinear()
         .domain([0, d3.max(data, function(d) {
-          return +d.frequency;
+          return +d.price;
         })])
         .range([height, 0]).nice();
-    var yAxis = svg.append("g");
+      var yAxis = svg.append("g");
 
-    // A function that builds the graph for a specific value of bin
-    function update() {
+      // A function that builds the graph for a specific value of bin
+      function update() {
 
-      // set the parameters for the histogram
-      var histogram = d3.histogram()
-          .value(function(d) { return d.key; })   // I need to give the vector of value
-          .domain(x.domain())  // then the domain of the graphic
-          ; // then the numbers of bins
+        // set the parameters for the histogram
+        var histogram = d3.histogram()
+            .value(function(d) { return d.key; })   // I need to give the vector of value
+            .domain(x.domain())  // then the domain of the graphic
+            ; // then the numbers of bins
 
-      // And apply this function to data to get the bins
+        // And apply this function to data to get the bins
 
-      // Y axis: update now that we know the domain
-      yAxis.transition().duration(1000).call(d3.axisLeft(y));
+        // Y axis: update now that we know the domain
+        yAxis.transition().duration(1000).call(d3.axisLeft(y));
 
-      // Join the rect with the bins data
-      var u = svg.selectAll(null)
-                  .data(data)
-                  .attr("class", "barchart-bar");
-      // Manage the existing bars and eventually the new ones:
-      u.enter()
-        .append("rect") // Add a new rect for each new elements
-        .on('mouseover', function(d) {
-              d3.select(this).style("fill", HOVERED_BAR_COLOR);
+        // Join the rect with the bins data
+        var u = svg.selectAll(null)
+                    .data(data)
+                    .attr("class", "barchart-bar");
+        // Manage the existing bars and eventually the new ones:
+        u.enter()
+          .append("rect") // Add a new rect for each new elements
+          .on('mouseover', function(d) {
+                d3.select(this).style("fill", HOVERED_BAR_COLOR);
+            })
+          .on('mouseout', function(d) {
+              d3.select(this).style("fill", DEFAULT_BAR_COLOR);
           })
-        .on('mouseout', function(d) {
-            d3.select(this).style("fill", DEFAULT_BAR_COLOR);
-        })
-        .merge(u) // get the already existing elements as well
-        .transition() // and apply changes to all of them
-        .duration(1000)
-          .attr("x", function(d) { return x(d.key); })
-          .attr("width", function(d) {
-            var w = x.bandwidth() - 1;
-            if (w < 0) { return 0;}
-            return w;
-          })
-          .attr("y", function(d) { return y(d.frequency);})
-          .attr("height", function(d) { return height - y(d.frequency);})
-          .call(d3.axisBottom(x))
-          .style("fill", "#69b3a2");
-      // If less bar in the new histogram, I delete the ones not in use anymore
-      u.exit().remove()
-    }
+          .merge(u) // get the already existing elements as well
+          .transition() // and apply changes to all of them
+          .duration(1000)
+            .attr("x", function(d) { return x(d.key); })
+            .attr("width", function(d) {
+              var w = x.bandwidth() - 1;
+              if (w < 0) { return 0;}
+              return w;
+            })
+            .attr("y", function(d) { return y(d.price);})
+            .attr("height", function(d) { return height - y(d.price);})
+            .call(d3.axisBottom(x))
+            .style("fill", "#69b3a2");
+        // If less bar in the new histogram, I delete the ones not in use anymore
+        u.exit().remove()
+      }
 
-    // Initialize with 20 bins
-    update()
+      // Initialize with 20 bins
+      update()
 
-  });
+    });
+  }
 
 }
