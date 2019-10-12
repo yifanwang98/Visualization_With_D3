12206df6917398b98fc3083d27f5582af3c71136
Yifan Wang <yifanwang07@163.com>
2019-10-11 22:18:52 -0400

    Correlation matrix implemented; scroll to top btn added; :)

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,11 +1,11 @@
 function plotHeatmap() {
   // set the dimensions and margins of the graph
   var w = document.documentElement.clientWidth;
-  var margin = {top:50, right: 100, bottom: 50, left: 100};
+  var margin = {top:30, right: 100, bottom: 50, left: 100};
   if (w < 600) {
     margin = {top: 30, right: 75, bottom: 50, left: 75};
   }
-  var width = Math.min(700, w);
+  var width = Math.min(640, w);
   var height = Math.min(550, w);
   width = width - margin.left - margin.right;
   height = height - margin.top - margin.bottom;
@@ -44,4 +44,66 @@ function plotHeatmap() {
   svg.append("g")
       .attr("class", "heatmapYAxis")
       .call(d3.axisLeft(y));
+
+  // Build color scale
+  var colorRange = ["#253ce7", "#fff", "#f00c00"];
+  var myColor = d3.scaleLinear()
+                  .range(colorRange)
+                  .domain([-1.0, 0, 1.0]);
+
+  //Read the data
+  d3.csv("data/heatmap.csv", function(data) {
+    console.log("Start rendering heatmap;");
+    // add the squares
+    svg.selectAll()
+      .data(data, function(d) { return ""+d.x+':'+d.y;})
+      .enter()
+      .append("rect")
+        .attr("x", function(d) { return x(d.x) })
+        .attr("y", function(d) { return y(d.y) })
+        .attr("width", x.bandwidth() )
+        .attr("height", y.bandwidth() )
+        .style("fill", function(d) {return myColor(d.corr)} );
+  });
+
+  // legend scale
+  var legend_top = 15;
+  var legend_height = 15;
+
+  d3.select("#heatmapLegend").selectAll("*").remove();
+  var legend_svg = d3.select("#heatmapLegend").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", legend_height + legend_top)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + ", " + legend_top + ")");
+
+  var defs = legend_svg.append("defs");
+
+  var gradient = defs.append("linearGradient")
+      .attr("id", "linear-gradient");
+
+  var stops = [{offset: 0, color: colorRange[0], value: -1},
+               {offset: .5, color: colorRange[1], value: 0},
+               {offset: 1, color: colorRange[2], value: 1}];
+
+  gradient.selectAll("stop")
+      .data(stops)
+    .enter().append("stop")
+      .attr("offset", function(d){ return (100 * d.offset) + "%"; })
+      .attr("stop-color", function(d){ return d.color; });
+
+  legend_svg.append("rect")
+            .attr("width", width)
+            .attr("height", legend_height)
+            .style("fill", "url(#linear-gradient)");
+
+  legend_svg.selectAll("text").data(stops)
+              .enter()
+              .append("text")
+                .attr("class", "heatmapLegendText")
+                .attr("x", function(d){ return width * d.offset; })
+                .attr("dy", -3)
+                .style("text-anchor", function(d, i){ return i == 0 ? "start" : i == 1 ? "middle" : "end"; })
+                .text(function(d, i){ return d.value.toFixed(2); });
+
 }
